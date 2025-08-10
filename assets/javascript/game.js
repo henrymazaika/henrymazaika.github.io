7e103a40904df2f94d766c967a74e436d5b55322
Henry Mazaika <henrymazaika@gmail.com>
2025-08-10 15:47:03 -0700

    added indigo

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,7 +11,7 @@ var doubleWord = ['a','b','c',
 				  'v','w','x',
 				  'y','z'];
 //Holds the all the words
-var wordBank =['orange','blue','green', 'yellow','violet','red','purple'];
+var wordBank =['orange','blue','green', 'yellow','violet','red','purple','indigo'];
 //Holds choosenWord
 var choosenWord = "";
 //Holds letters in word
@@ -37,7 +37,7 @@ function reset()
 	lettersInWord = choosenWord.split('');
 	//Get the number of blanks
 	numBlanks = lettersInWord.length;
-	
+
 	//RESET
 	//===========================================================
 	letterGuessed = 0;
@@ -65,7 +65,7 @@ function startGame()
 	lettersInWord = choosenWord.split('');
 	//Get the number of blanks
 	numBlanks = lettersInWord.length;
-	
+
 	//RESET
 	//===========================================================
 	rightGuessCounter = 0;
@@ -89,7 +89,7 @@ function startGame()
 		document.getElementById('wordToGuess').innerHTML = blanksAndSuccesses;
 	}
 
-	//Changes HTML 
+	//Changes HTML
 	document.getElementById('wordToGuess').innerHTML = blanksAndSuccesses.join(' ');
 	document.getElementById('numGuesses').innerHTML = guessesLeft;
 	document.getElementById('winCounter').innerHTML = winCount;
@@ -105,10 +105,10 @@ function startGame()
 function compareLetters(userKey)
 {
 				console.log('WORKING!');
-				//If user key exist in choosen word then perform this function 
+				//If user key exist in choosen word then perform this function
 				if(choosenWord.indexOf(userKey) > -1)
 				{
-					//Loops depending on the amount of blanks 
+					//Loops depending on the amount of blanks
 					for(var i = 0; i < numBlanks; i++)
 					{
 						//Fills in right index with user key
@@ -117,7 +117,7 @@ function compareLetters(userKey)
 							rightGuessCounter++;
 							blanksAndSuccesses[i] = userKey;
 							document.getElementById('wordToGuess').innerHTML = blanksAndSuccesses.join(' ');
-						}	
+						}
 					}
 					//Test / Debug
 					console.log(blanksAndSuccesses);
@@ -134,16 +134,16 @@ function compareLetters(userKey)
 					console.log('Wrong Letters = ' + wrongLetters);
 					console.log('Guesses left are ' + guessesLeft);
 				}
-			
-			
-		
+
+
+
 }
 function winLose()
 {
 	// When number blanks if filled with right words then you win
 	if(rightGuessCounter === numBlanks)
 	{
-		//Counts Wins 
+		//Counts Wins
 		winCount++;
 		//Changes HTML
 		document.getElementById('winCounter').innerHTML = winCount;
@@ -163,7 +163,7 @@ function winLose()
 }
 
 //MAIN PROCCESS
-//-------------------------------------------	
+//-------------------------------------------
 //Initiates the Code
 startGame();
 
@@ -172,7 +172,7 @@ document.onkeyup = function(event)
 	test = true;
 	var letterGuessed = event.key;
 	for(var i = 0; i < doubleWord.length; i++)
-	{	
+	{
 		if(letterGuessed === doubleWord[i] && test === true)
 		{
 			var spliceDword = doubleWord.splice(i,1);
@@ -183,6 +183,6 @@ document.onkeyup = function(event)
 			compareLetters(letterGuessed);
 			winLose();
 		}
-	}		
-		
+	}
+
 }
